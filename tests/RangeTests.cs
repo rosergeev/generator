f402eb27a2f5ae069c9ce9ae7a80d879708919cb
rosergeev <rosergeev@gmail.com>
2021-07-22 20:03:42 +0300

    Add sort test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -12,5 +12,16 @@ namespace tests
             var generated = range.Of(() => "");
             Assert.Equal(3, generated.Count());
         }
+
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new api.Controllers.Range { Count = 3, Sort = true };
+            var values = new[] { "a", "c", "b" };
+            var counter = 0;
+            var generated = range.of(() => values[counter++]);
+
+            Assert.Equal(new[] { "a", "b", "c" }, generated.ToArray());
+        }
     }
 }
