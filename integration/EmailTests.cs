cb342ab5269786e4e0c681508bb5e484f2fbb511
rosergeev <rosergeev@gmail.com>
2021-07-30 21:32:45 +0300

    Fix test

--- integration/EmailTests.cs
+++ integration/EmailTests.cs
@@ -47,7 +47,7 @@ namespace integration
                     .Which.Should().BeOfType<JArray>()
                     .Which.First.Should().HaveElement("Raw")
                     .Which.Should().HaveElement("From")
-                    .Which.Should().HaveValue("enerator@generator.com");
+                    .Which.Should().HaveValue("generator@generator.com");
             }
         }
     }
